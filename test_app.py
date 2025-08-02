23a9efd2ae319003ade66a2b4401adb0c2894e6a
santoshnog <connect2san.nog@gmail.com>
2025-08-02 13:00:59 +0530

    third commit

--- test_app.py
+++ test_app.py
@@ -1,8 +1,8 @@
 from app import lwinfo
 from app import lwphone
 
-def test_lwinfo()
+def test_lwinfo():
     assert lwinfo() == "Welcome to Linux"
 	
-def test_lwphone()
+def test_lwphone():
     assert lwphone() == "+91-9740254232"
